84c31cc305a03d37df507298f85d61f6bf7a7109
redcreeper14385 <noahkoro@gmail.com>
2020-08-27 15:06:50 +0100

    Removed unnecessary imports

--- src/main/java/mounderfod/moundertweaks/util/config/ClientConfig.java
+++ src/main/java/mounderfod/moundertweaks/util/config/ClientConfig.java
@@ -2,7 +2,6 @@ package mounderfod.moundertweaks.util.config;
 
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui.Tooltip;
 
 @Config(name = "client")
