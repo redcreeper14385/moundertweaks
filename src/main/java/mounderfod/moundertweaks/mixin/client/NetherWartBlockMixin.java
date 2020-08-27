1965c92a2e3cd3afd1c2b2df6e0eb1487c5d2591
SD <harrypotter.starwars.shrish@gmail.com>
2020-08-27 21:08:47 +0530

    Merge
     Onbranch fixit
     Changes to be committed:
    	new file:   src/main/java/mounderfod/moundertweaks/mixin/client/NetherWartBlockMixin.java
    	modified:   src/main/resources/moundertweaks.mixins.json

---
+++ src/main/java/mounderfod/moundertweaks/mixin/client/NetherWartBlockMixin.java
@@ -0,0 +1,9 @@
+package mounderfod.moundertweaks.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.NetherWartBlock;
+
+@Mixin(NetherWartBlock.class)
+public class NetherWartBlockMixin {
+}
